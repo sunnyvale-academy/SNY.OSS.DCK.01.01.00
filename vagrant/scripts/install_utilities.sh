0f17d148fc32f7e3bea23a27e80588d2322c6252
Denis Maggiorotto <denis.maggiorotto@sunnyvale.it>
2021-06-22 13:00:58 +0000

    Install JDK11

--- vagrant/scripts/install_utilities.sh
+++ vagrant/scripts/install_utilities.sh
@@ -35,3 +35,11 @@ sudo apt-get -y install dos2unix
 # Goimports
 #/usr/local/go/bin/go get golang.org/x/tools/cmd/goimports
 
+sudo apt-get install -y wget apt-transport-https gnupg
+wget https://adoptopenjdk.jfrog.io/adoptopenjdk/api/gpg/key/public
+gpg --no-default-keyring --keyring ./adoptopenjdk-keyring.gpg --import public
+gpg --no-default-keyring --keyring ./adoptopenjdk-keyring.gpg --export --output adoptopenjdk-archive-keyring.gpg 
+rm adoptopenjdk-keyring.gpg
+sudo mv adoptopenjdk-archive-keyring.gpg /usr/share/keyrings 
+echo "deb [signed-by=/usr/share/keyrings/adoptopenjdk-archive-keyring.gpg] https://adoptopenjdk.jfrog.io/adoptopenjdk/deb xenial main" | sudo tee /etc/apt/sources.list.d/adoptopenjdk.list
+sudo apt-get install -y adoptopenjdk-11-hotspot
